0b39d569230d1a9989eba5da2f5f3b39cf0bc97e
Dmytro Vorona <alendit@gmail.com>
2012-09-05 16:13:07 +0200

    Apache server restart added

--- src/exedjango/fabfile.py
+++ src/exedjango/fabfile.py
@@ -2,7 +2,7 @@ from fabric.api import *
 
 env.use_ssh_config = True
 env.hosts = ['creyoco_dev']
-env.code_dir = '/home/dimitri/creyoco/src/exedjango/'
+env.code_dir = '/home/dimitri/creyoco/src/exedjango'
 
 
 def pull():
@@ -15,3 +15,9 @@ def push():
 def update():
     execute(push)
     execute(pull)
+    
+def copy_site():
+    run("sudo ln -sf {0}/apache/001-creyoco /etc/apache2/sites-enabled/".format(env.code_dir))
+    
+def restart():
+    run("sudo service apache2 restart")
