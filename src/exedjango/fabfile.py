aece62483263b373a33182c7966986329224d3fd
Dmytro Vorona <alendit@gmail.com>
2012-09-05 15:51:55 +0200

    Basic fabfile added

---
+++ src/exedjango/fabfile.py
@@ -0,0 +1,10 @@
+from fabric.api import *
+
+env.use_ssh_config = True
+env.hosts = ['creyoco_dev']
+env.code_dir = '/home/dimitri/creyoco/src/exedjango/'
+
+
+def pull():
+    with cd(env.code_dir):
+        run('git pull')
