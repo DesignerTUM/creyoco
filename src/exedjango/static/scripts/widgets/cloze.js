f3d941dfec556086d9d466b4751cc935ce845145
DesignerTUM <design.mz@tum.de>
2012-11-05 16:33:21 +0100

    Line endings

---
+++ src/exedjango/static/scripts/widgets/cloze.js
@@ -0,0 +1,45 @@
+jQuery(document).ready(function(){
+	$(".cloze_submit").live("click", submit_cloze);
+	$(".cloze_restart").live("click", function(e){
+		$(this).parent().hide();
+		$(this).parent().parent()
+		.find(".cloze_gap").each(function() {
+			$(this).val("");
+			$(this).css("backgroundColor", "white");	
+		});
+		e.preventDefault();
+	});
+	$(".cloze_show_answers").live("click", function(e) {
+		$(this).parent().parent().find(".cloze_gap").each(function(){
+			$(this).val(get_right_answer($(this)));
+			$(this).css("backgroundColor", "green");
+		});
+		e.preventDefault();
+	});
+});
+
+function submit_cloze(e){
+	var gap_count = 0;
+	var right_answer_count = 0;
+	$(this).parent().find(".cloze_gap").each(function (){
+		gap_count++;
+		var gap_id = /gap_(.*)$/.exec($(this).attr("id"))[1];
+		var right_answer = get_right_answer($(this));
+		if ($(this).val() == right_answer){
+			$(this).css("backgroundColor", "green");
+			right_answer_count++;
+		}
+		else {
+			$(this).css("backgroundColor", "red");		
+		}
+	});
+	var result_string = "Your score is " + right_answer_count + "/" + gap_count + "."; 
+	$(this).next().find(".cloze_result_text").text(result_string);
+	$(this).next(".cloze_result").show();
+	e.preventDefault();
+}
+
+function get_right_answer($gap){
+	var gap_id = /gap_(.*)$/.exec($gap.attr("id"))[1];
+	return $gap.parent().parent().find("#answer_" + gap_id).val();
+}
