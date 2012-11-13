ca0ad2ffb8c35c7e117627656d64b952940b5fac
Dmytro Vorona <alendit@gmail.com>
2012-11-13 11:45:42 +0100

    New line added to the end of the file

--- src/exedjango/fabfile.py
+++ src/exedjango/fabfile.py
@@ -8,16 +8,20 @@ env.code_dir = '/home/dimitri/creyoco/src/exedjango'
 def pull():
     with cd(env.code_dir):
         run('git pull')
-        
+
+
 def push():
     local('git push')
-    
+
+
 def update():
     execute(push)
     execute(pull)
-    
+
+
 def copy_site():
     run("sudo ln -sf {0}/apache/001-creyoco /etc/apache2/sites-enabled/".format(env.code_dir))
-    
+
+
 def restart():
     run("sudo service apache2 restart")
