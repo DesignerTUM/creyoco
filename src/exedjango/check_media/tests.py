362fb71e2df8f9ff0aa6c12bdead625e0694ceca
Dmytro Vorona <alendit@gmail.com>
2013-01-29 17:26:36 +0100

    Tests added to check_media app

--- src/exedjango/check_media/tests.py
+++ src/exedjango/check_media/tests.py
@@ -6,11 +6,41 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.contrib.auth.models import User
+import os
+from django.test import Client
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class BasicTest(TestCase):
+
+    TEST_USER = 'user'
+    TEST_PASSWORD = 'password'
+    CONTENT = "test test test"
+    FILE_NAME = "test.txt"
+
+    def setUp(self):
+        user = User.objects.create_user(username=self.TEST_USER,
+                               password=self.TEST_PASSWORD)
+        media_path = user.get_profile().media_path
+        file(os.path.join(media_path, self.FILE_NAME), 'w').write(self.CONTENT)
+        self.file_url = user.get_profile().media_url + self.FILE_NAME
+        self.client = Client()
+
+    def test_basic_access(self):
         """
         Tests that 1 + 1 always equals 2.
         """
-        self.assertEqual(1 + 1, 2)
+        self.client.login(username=self.TEST_USER,
+                          password=self.TEST_PASSWORD)
+        response = self.client.get(self.file_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.CONTENT)
+
+    def test_403_on_illegal(self):
+        wrong_name = 'user2'
+        wrong_password = 'password2'
+        wrong_user = User.objects.create_user(username=wrong_name,
+                                              password=wrong_password)
+        self.client.login(username=wrong_name, password=wrong_password)
+        response = self.client.get(self.file_url)
+        self.assertEqual(response.status_code, 403)
