7da5f199b8bce8e6d2925f8125b31476e00dccf6
Dmytro Vorona <alendit@gmail.com>
2013-06-04 16:27:47 +0200

    Switched db to mysql

--- src/exedjango/deployment_settings.py
+++ src/exedjango/deployment_settings.py
@@ -5,27 +5,27 @@ from settings import *
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-#DATABASES = {
-#        'default' : {
-#                'ENGINE' : 'django.db.backends.mysql',
-#                'NAME' : 'exedjangodb',
-#                'USER' : 'root',
-#                'PASSWORD' : 'ssl20qwerty',
-#                'HOST' : 'localhost',
-#                'PORT' : '',
-#        }
-#}
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/dimitri/db/sqlite.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+       'default' : {
+               'ENGINE' : 'django.db.backends.mysql',
+               'NAME' : 'creyoco',
+               'USER' : 'root',
+               'PASSWORD' : 'ssl20qwerty',
+               'HOST' : 'localhost',
+               'PORT' : '',
+       }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': '/home/dimitri/db/sqlite.db',                      # Or path to database file if using sqlite3.
+#         'USER': '',                      # Not used with sqlite3.
+#         'PASSWORD': '',                  # Not used with sqlite3.
+#         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#     }
+# }
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '129.187.81.136']
 
