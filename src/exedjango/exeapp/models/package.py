DesignerTUM/creyoco

commit 0abac908c500c268dc5b14dcd4b054dc26559893

Fixed style change bug

src/exedjango/exeapp/models/package.py
@@ -17,6 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 # ===========================================================================
+import os
 from django.db import models
 import datetime
 
@@ -27,6 +28,7 @@ i.e. the "package".
 
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from django.conf import settings
 
 
 import logging
@@ -373,6 +375,12 @@ i.e. the "package".
         node.save()
 
     def set_style(self, style):
+        if not style in [os.path.basename(style) for style in \
+              os.listdir(settings.STYLE_DIR) \
+              # style dir has to be joined because of a bug on windows
+              # with abapath resolving
+              if os.path.isdir(os.path.join(settings.STYLE_DIR, style))]:
+            raise ValueError("Style {} cannot be found".format(style))
         self.style = style
         self.save()
 
@@ -585,7 +593,7 @@ i.e. the "package".
     def findResourceByName(self, queryName):
         """
         Support for merging, and anywhere else that unique names might be
-        checked before actually comparing against the files (as will be 
+        checked before actually comparing against the files (as will be
         done by the resource class itself in its _addOurselvesToPackage() )
         """
         foundResource = None

src/exedjango/exeapp/static/scripts/eyecandy.js
@@ -172,7 +172,6 @@ $(document).ready(function() {
 		$('.theme').removeClass('selected');
 		$(this).addClass('selected');
 
-		handle_select_style();
 		update_preview();
 	});
 	/*$('#download').click( function(){

src/exedjango/exeapp/tests.py
@@ -311,6 +311,9 @@ view, this tests should be also merged'''
         self.c = Client()
         _create_basic_database()
         self.c.login(username=TEST_USER, password=TEST_PASSWORD)
+        self.s = TestingServiceProxy(self.c,
+                        reverse("jsonrpc_mountpoint"),
+                        version="2.0")
         self.package = Package.objects.get(pk=self.TEST_PACKAGE_ID)
         self.root = self.package.root
 
@@ -486,6 +489,10 @@ view, this tests should be also merged'''
             simplejson.loads(link_list)
         except:
             raise AssertionError("Couldn't parse %s" % link_list)
+
+    def test_style_invalid(self):
+        self.assertRaises(ValueError, self.s.package.set_package_style,
+                          1, 1, 'BS')
 
 
 class ExportTestCase(TestCase):

commit c3b42fc5483dcb5b1f7352d4259c14aedf216725

Fixed style change bug introduced in 0abac90

src/exedjango/exeapp/models/package.py
@@ -375,11 +375,11 @@ i.e. the "package".
         node.save()
 
     def set_style(self, style):
-        if not style in [os.path.basename(style) for style in \
+        if not style in [os.path.basename(exist_style) for exist_style in \
               os.listdir(settings.STYLE_DIR) \
               # style dir has to be joined because of a bug on windows
               # with abapath resolving
-              if os.path.isdir(os.path.join(settings.STYLE_DIR, style))]:
+              if os.path.isdir(os.path.join(settings.STYLE_DIR, exist_style))]:
             raise ValueError("Style {} cannot be found".format(style))
         self.style = style
         self.save()