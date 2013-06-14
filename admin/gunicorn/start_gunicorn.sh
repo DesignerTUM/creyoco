b48ffbd63baa5454b28b56de424df8bb78b8e0ec
Dmytro Vorona <alendit@gmail.com>
2013-06-14 12:27:53 +0200

    Updated path to settings

--- admin/gunicorn/start_gunicorn.sh
+++ admin/gunicorn/start_gunicorn.sh
@@ -13,5 +13,5 @@ test -d $LOGDIR || mkdir -p $LOGDIR
 touch $LOGFILE
 cd $DJANGO_PATH
 gunicorn_django -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug \
-    --log-file=$LOGFILE --settings=deployment_settings 2>$LOGFILE
+    --log-file=$LOGFILE --settings=exedjango.deployment_settings 2>$LOGFILE
 echo "Gunicorn started"
