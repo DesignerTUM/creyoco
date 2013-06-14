3926e67dfc22d302bfce81e6eb9e8f12421fe2c2
Dmytro Vorona <alendit@gmail.com>
2013-06-14 12:29:19 +0200

    Python path added to gunicorn start script

--- admin/gunicorn/start_gunicorn.sh
+++ admin/gunicorn/start_gunicorn.sh
@@ -13,5 +13,6 @@ test -d $LOGDIR || mkdir -p $LOGDIR
 touch $LOGFILE
 cd $DJANGO_PATH
 gunicorn_django -w $NUM_WORKERS --user=$USER --group=$GROUP --log-level=debug \
-    --log-file=$LOGFILE --settings=exedjango.deployment_settings 2>$LOGFILE
+    --log-file=$LOGFILE --pythonpath=. --settings=exedjango.deployment_settings \
+    2>$LOGFILE
 echo "Gunicorn started"
