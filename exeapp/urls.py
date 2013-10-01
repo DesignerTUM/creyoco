985b43616d4d4f364a1aa3bfe7959f0ce70d984f
Dmytro Vorona <alendit@gmail.com>
2013-10-01 09:20:05 +0200

    Reformated url.py

--- exeapp/urls.py
+++ exeapp/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns,url, include
+from django.conf.urls import patterns, url, include
 from django.conf import settings
 from django.views.generic import RedirectView
 from jsonrpc import jsonrpc_site
@@ -7,20 +7,21 @@ from jsonrpc.views import browse as json_browse
 import exeapp.views.handlers.package_rpc
 import exeapp.views.handlers.package_outline_rpc
 
-
 from exeapp.views import main
 
 urlpatterns = patterns('',
-            # (r'', 'exeapp.views.main'),
-            (r'^$', main.main),
+                       (r'^$', main.main),
 
-            url(r'^json/$', jsonrpc_site.dispatch, name="jsonrpc_mountpoint"),
-            (r'package/$', RedirectView.as_view(url='/exeapp/')),
-            (r'package/(?P<package_id>\d+)/', include('exeapp.package_urls')),
-            (r'pages', include('django.contrib.flatpages.urls')),
-        )
+                       url(r'^json/$', jsonrpc_site.dispatch,
+                           name="jsonrpc_mountpoint"),
+                       (r'package/$', RedirectView.as_view(url='/exeapp/')),
+                       (r'package/(?P<package_id>\d+)/',
+                        include('exeapp.package_urls')),
+                       (r'pages', include('django.contrib.flatpages.urls')),
+                    )
 
 if settings.DEBUG:
     urlpatterns += patterns('',
-            url(r'^json/browse/', json_browse, name='jsonrpc_browser'),
-        )
+                            url(r'^json/browse/', json_browse,
+                                name='jsonrpc_browser'),
+    )
