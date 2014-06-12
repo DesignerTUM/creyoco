ceccae2e9cf5748736da932a500930cb957a4fb4
Dmytro Vorona <alendit@gmail.com>
2014-06-12 13:52:13 +0200

    Strict decorator fixed.

--- strict_filebrowser/sites.py
+++ strict_filebrowser/sites.py
@@ -1,42 +1,46 @@
 """Custom filebrowser implementation for creyoco"""
-from django.core.files.storage import DefaultStorage
-from django.http import HttpResponseForbidden, QueryDict
-from filebrowser.sites import FileBrowserSite
-from filebrowser.settings import *
-from filebrowser.actions import *
-
 import os
 from functools import wraps
 
+from django.core.files.storage import DefaultStorage
+from django.http import HttpResponseForbidden
+from django.views.decorators.cache import never_cache
+
+from filebrowser.sites import FileBrowserSite
+import filebrowser.sites
+from filebrowser.settings import *
+from filebrowser.actions import *
+
 
 def strict_decorator(func):
-        """Takes a method and inserts user name into the query path"""
-        @wraps(func)
-        def wrapper(request):
-            if not request.user.is_authenticated():
-                return HttpResponseForbidden("Please log in")
-            username = request.user.username
-            target_dir = request.GET.get("dir", '')
-            if not target_dir.startswith(username):
-                new_query_dict = request.GET.copy()
-                new_query_dict['dir'] = os.path.join(username, target_dir)
-                request.GET = new_query_dict
-            return func(request)
-        return wrapper
+    """Takes a method and inserts user name into the query path"""
+
+    @wraps(func)
+    def wrapper(request):
+        if not request.user.is_authenticated():
+            return HttpResponseForbidden("Please log in")
+        username = request.user.username
+        target_dir = request.GET.get("dir", '')
+        if not target_dir.startswith(username):
+            new_query_dict = request.GET.copy()
+            new_query_dict['dir'] = os.path.join(username, target_dir)
+            request.GET = new_query_dict
+        return func(request)
+
+    return wrapper
+
+
+def filebrowser_view_override(view):
+    """ Add a strict decorator """
+    return strict_decorator(never_cache(view))
+
+# Monkey patch the view dispatcher
+filebrowser.sites.filebrowser_view = filebrowser_view_override
 
 
 class StrictFilebrowserSite(FileBrowserSite):
-    """Overrides filebrowser's default directory selection.
-    Bases it on the user name"""
-
-    def browse(self, *args, **kwargs):
-        return super(StrictFilebrowserSite, self).browse(*args, **kwargs)
-
-    def filebrowser_view(self, view):
-        super_view = super(StrictFilebrowserSite, self).filebrowser_view(
-            view
-        )
-        return strict_decorator(view)
+    """Is there just so the app works"""
+    pass
 
 
 storage = DefaultStorage()
