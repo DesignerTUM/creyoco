587ee047e789fecdd732e6909dc0fb7878b2aad3
abuzakaria <zakariatanim@gmail.com>
2014-07-31 09:58:46 +0200

    implemented wikipedia library functions

--- exeapp/models/idevices/wikiidevice.py
+++ exeapp/models/idevices/wikiidevice.py
@@ -1,6 +1,6 @@
 import re
 import sys
-
+import wikipedia
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from bs4 import BeautifulSoup
@@ -64,77 +64,10 @@ within Wikipedia."""))
         """
         Load the article from Wikipedia
         """
-        self.articleName = title
-        self.site = self.language + "." + self.site
-        url = ""
-        title = quote(title.replace(" ", "_").encode('utf-8'))
-        try:
-            url = (self.site or self.ownUrl)
-            if not url.endswith('/') and title != '':
-                url += '/'
-            if '://' not in url:
-                url = 'http://' + url
-            url += title
-            net = urlopen(url)
-            page = net.read().decode("utf-8")
-            net.close()
-        except IOError:
-            self.content = _(
-                "Unable to download from %s <br/>Please check the spelling "
-                "and connection and try again.") % url
-            return
+        self.article_name = title
+        page = wikipedia.page(title)
+        self.content = page.html()
 
-        # FIXME avoid problems with numeric entities in attributes
-        page = page.replace('&#160;', '&nbsp;')
-
-        soup = BeautifulSoup(page)
-        content = soup.find('div', {'id': "content"})
-
-        # remove the wiktionary, wikimedia commons, and categories boxes
-        #  and the protected icon and the needs citations box
-        if content:
-            content['id'] = 'wiki_content'
-            infoboxes = content.findAll('div',
-                                        {'class': 'infobox sisterproject'})
-            [infobox.extract() for infobox in infoboxes]
-            catboxes = content.findAll('div', {'id': 'catlinks'})
-            [catbox.extract() for catbox in catboxes]
-            amboxes = content.findAll('table',
-                                      {'class': re.compile(r'.*\bambox\b.*')})
-            [ambox.extract() for ambox in amboxes]
-            protecteds = content.findAll('div', {'id': 'protected-icon'})
-            [protected.extract() for protected in protecteds]
-        else:
-            content = soup.first('body')
-
-        if not content:
-            self.content = _(
-                "Unable to download from %s <br/>Please check the spelling "
-                "and connection and try again.") % url
-            # set the other elements as well
-            return
-
-        bits = url.split('/')
-        netloc = '%s//%s' % (bits[0], bits[2])
-        self.content = self.reformatArticle(netloc, str(content))
-        # now that these are supporting images, any direct manipulation
-        # of the content field must also store this updated information
-        # into the other corresponding fields of TextAreaField:
-        # (perhaps eventually a property should be made for TextAreaField
-        #  such that these extra set's are not necessary, but for now, here:)
-
-    def reformatArticle(self, netloc, content):
-        """
-        Changes links, etc
-        """
-        content = re.sub(r'href="/', r'href="%s/' % netloc, content)
-        content = re.sub(
-            r'<(span|div)\s+(id|class)="(editsection|jump-to-nav)".*?</\1>',
-            '', content)
-        # TODO Find a way to remove scripts without removing newlines
-        content = content.replace("\n", " ")
-        content = re.sub(r'<script.*?</script>', '', content)
-        return content
 
     class Meta:
         app_label = "exeapp"
