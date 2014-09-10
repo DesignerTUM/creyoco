361d91bb5bc64229cd8fe69a84b1873fc827b419
Dmytro Vorona <alendit@gmail.com>
2014-09-10 14:48:18 +0200

    Fixed multichoice for unchecked.

--- exeapp/static/scripts/blocks/multichoice.js
+++ exeapp/static/scripts/blocks/multichoice.js
@@ -1,15 +1,25 @@
-multichoice = {
+var multichoice = {
     init: function () {
+        "use strict";
         $(document).ready(function () {
             $(".check_multichoice").off("click").on("click", function (e) {
                 var options = $(this).parent().find("input[type='radio'], input[type='checkbox']");
                 var wrong = false;
                 var result_el = $(this).parent().find(".result");
                 $.each(options, function (n, el) {
-                    $(el).next().removeClass("wrong_answer");
-                    if (($(el).attr("data-right") == "true") != $(el).prop("checked")) {
-                        $(el).next().addClass('wrong_answer');
-                        wrong = true;
+                    $(el).next()
+                        .removeClass("wrong_answer")
+                        .removeClass("right_answer");
+                    if ($(el).attr("data-right") === "true") {
+                        $(el).next().addClass('right_answer');
+                        if (!($(el).prop("checked"))) {
+                            wrong = true;
+                        }
+                    } else {
+                        if ($(el).prop("checked")) {
+                            $(el).next().addClass('wrong_answer');
+                            wrong = true;
+                        }
                     }
                 });
                 if (wrong) {
