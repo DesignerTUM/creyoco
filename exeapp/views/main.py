3f5f9b62bdf0b90d425d0fcd88a98bf068e70563
Dmytro Vorona <alendit@gmail.com>
2014-09-12 14:40:12 +0200

    Fixed faulty merge

--- exeapp/views/main.py
+++ exeapp/views/main.py
@@ -1,33 +1,34 @@
 '''Main view for a user. Handles both GET/POST request and rpc calls'''
-
-from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.db.models import Q
+from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
-from jsonrpc import jsonrpc_method
-
 from django.contrib import messages
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
+
+from jsonrpc import jsonrpc_method
 from exeapp.views import upload_file_form
 from exeapp.models import Package, User
 from exeapp.shortcuts import get_package_by_id_or_error
-from exeapp.views.export.exporter_factory import exporter_map
+from views.export.exporter_factory import exporter_map
 
 
 @login_required
 def main(request):
     '''Serve the main page with a list of packages.
     TODO: Use a generic view'''
-    user = User.objects.get(username=request.user.username)
-    package_list = Package.objects.filter(user=user)
-    exporter_type_title_map = dict(((export_type, exporter.title) \
-                    for export_type, exporter in list(exporter_map.items())))
-    form = upload_file_form.UploadFileForm()
+    package_list = Package.objects.filter(
+            Q(user=request.user) |
+            Q(collaborators__pk__contains=request.user.pk)
+    )
+    exporter_type_title_map = dict(((export_type, exporter.title)
+        for export_type, exporter in list(exporter_map.items())))
 
     return render_to_response(
         'main.html',
         locals(),
         context_instance=RequestContext(request))
+
 
 @login_required
 def upload_zip(request):
