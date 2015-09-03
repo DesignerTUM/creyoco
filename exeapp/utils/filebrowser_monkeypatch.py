d638e92eab3b5bbb135e7f801a1dc960a2225203
zakaria <zakariatanim@gmail.com>
2015-09-03 16:28:07 +0200

    strictfilebrowser functions for filebrowser

--- exeapp/utils/filebrowser_monkeypatch.py
+++ exeapp/utils/filebrowser_monkeypatch.py
@@ -3,9 +3,7 @@ from django.views.decorators.cache import never_cache
 from django.views.decorators.clickjacking import xframe_options_sameorigin
 from django.views.decorators.csrf import csrf_exempt
 from filebrowser.decorators import path_exists, file_exists
-from filebrowser.sites import filebrowser_view
-from strict_filebrowser.sites import site
-
+from strict_filebrowser.sites import site, filebrowser_view_override
 
 def get_urls(self):
     "URLs for a filebrowser.site"
@@ -18,22 +16,18 @@ def get_urls(self):
     # filebrowser urls (views)
     urlpatterns = patterns(
         '',
-        url(r'^browse/$', path_exists(self, filebrowser_view(self.browse)), name="fb_browse"),
-        url(r'^createdir/', path_exists(self, filebrowser_view(self.createdir)), name="fb_createdir"),
-        url(r'^upload/', path_exists(self, filebrowser_view(self.upload)), name="fb_upload"),
-        url(r'^delete_confirm/$', file_exists(self, path_exists(self, filebrowser_view(self.delete_confirm))), name="fb_delete_confirm"),
-        url(r'^delete/$', file_exists(self, path_exists(self, filebrowser_view(self.delete))), name="fb_delete"),
-        url(r'^detail/$', file_exists(self, path_exists(self, filebrowser_view(self.detail))), name="fb_detail"),
-        url(r'^version/$', file_exists(self, path_exists(self, filebrowser_view(self.version))), name="fb_version"),
+        url(r'^browse/$', path_exists(self, filebrowser_view_override(self.browse)), name="fb_browse"),
+        url(r'^createdir/', path_exists(self, filebrowser_view_override(self.createdir)), name="fb_createdir"),
+        url(r'^upload/', path_exists(self, filebrowser_view_override(self.upload)), name="fb_upload"),
+        url(r'^delete_confirm/$', file_exists(self, path_exists(self, filebrowser_view_override(self.delete_confirm))), name="fb_delete_confirm"),
+        url(r'^delete/$', file_exists(self, path_exists(self, filebrowser_view_override(self.delete))), name="fb_delete"),
+        url(r'^detail/$', file_exists(self, path_exists(self, filebrowser_view_override(self.detail))), name="fb_detail"),
+        url(r'^version/$', file_exists(self, path_exists(self, filebrowser_view_override(self.version))), name="fb_version"),
         url(r'^upload_file/$', csrf_exempt(self._upload_file), name="fb_do_upload"),
     )
     return urlpatterns
 
-def filebrowser_view(view):
-    "Only let staff browse the files"
-    return never_cache(xframe_options_sameorigin(view))
 
 
 def monkey_patch_url():
     site.get_urls = types.MethodType(get_urls, site)
-    site.filebrowser_view = types.MethodType(filebrowser_view, site)
