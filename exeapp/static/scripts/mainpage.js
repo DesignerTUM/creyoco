d0a980fbcc619ca25dfc2306e0173d8ef4af04a0
Dimitri Vorona <vorona@in.tum.de>
2015-09-10 14:43:08 +0200

    Removed mathjax from mainpage.js.

--- exeapp/static/scripts/mainpage.js
+++ exeapp/static/scripts/mainpage.js
@@ -57,7 +57,7 @@ NOT_IMPLEMENTED = "This function is not implemented yet."
 SAVE_DIRTY_PACKAGE = "Package has been changed. Do you want to save it, before you leave?"
 
 require(['jquery', "common", "eyecandy", "wamp_handler", 'jquery-pjax', 'jquery-cookie', 'jquery-jsonrpc', "jstree", 'jquery-modal', 'modernizr',
-    'multichoice', 'feedback', 'cloze', 'filebrowser', 'mathjax'],
+    'multichoice', 'feedback', 'cloze', 'filebrowser'],
     function ($, common, eyecandy, wamp_handler) {
         "use strict";
         // set crfs cookie
