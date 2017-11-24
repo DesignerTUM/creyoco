e9bccfc1071e951fdaee9833dbbff2d851eba479
Dimitri Vorona <vorona@in.tum.de>
2017-11-24 22:53:53 +0100

    Use insertrule to support firefox

--- exeapp/static/css/styles/bmz-responsive/js/placement.js
+++ exeapp/static/css/styles/bmz-responsive/js/placement.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     var header_height = $('header').outerHeight(true);
-    document.styleSheets[0].addRule(
-        'header.affix ~ section.content > .middle',
-        'margin-top: ' + header_height + 'px'
+    document.styleSheets[0].insertRule(
+        ('header.affix ~ section.content > .middle {' +
+        'margin-top: ' + header_height + 'px' +
+        '}')
     );
 });
